4a6ab4a5f5941bce501ea8dae1412411465787d4
neon2k21 <roman_2001@icloud.com>
2024-10-02 18:20:47 +0500

    dkjgdfgd

--- App.js
+++ App.js
@@ -8,27 +8,50 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
 
+  let buttons = [
+                  [{x:0,y:0}, {x:0,y:1}, {x:0,y:2}], 
+                  [{x:1,y:0}, {x:1,y:1}, {x:1,y:2}],
+                  [{x:2,y:0}, {x:2,y:1}, {x:2,y:2}]
+                ]
+  
+  const [win, SetWin] = useState(false)
+  const [queue, SetQueue] = useState(false)
   const [symbol, setSymbol] = useState("X")
+  const [player1_score, setplayer1_score] = useState(0)
+  const [player2_score, setplayer2_score] = useState(0)
+
+  // const checkwin = () => {
+    
+  //   if(buttons[0][0][0] && buttons[0][0][1] && buttons[0][0][2])
+
+  // }
+
   
-  
+  const count_scores = () =>{
+    setplayer1_score(player1_score++)
+    setplayer2_score(player2_score++)
+  }
 
   const click = () =>{
-    if (symbol == 'x') {
+    if (!queue) {
+      SetQueue(true)
       setSymbol('O')
-      console.warn(symbol)
     } 
     else {
       setSymbol('x')
-      console.warn(symbol)
-      console.log(symbol)
+      SetQueue(false)
     }
   }
 
+  
 
   return (
     <View>
       
       <View style = {{backgroundColor:'gray', width: '100%', height:'80%'}}>
+        <Text>
+          Счет: {player1_score} - {player2_score}
+        </Text>
       <View style = {{width: '100%', height:'40%'}}>
           <TouchableOpacity onPress={click}>
             <Button_tictactoe id = {0} text = {symbol} />
